496211e531510f388227b56a843c2b651fcf6148
Farhan Sabbir Siddique <fsabbir@gmail.com>
2024-09-27 23:07:14 +0600

    renamed object to match model

--- main.dart
+++ main.dart
@@ -11,9 +11,9 @@ class Car {
 }
 
 void main() {
-  Car car1 = Car("Toyota", "Corolla", 2015);
-  print("Brand: ${car1.brand}");
-  print("Model: ${car1.model}");
-  print("Year: ${car1.year}");
-  print("Car Age: ${car1.carAge()} years");
+  Car toyota = Car("Toyota", "Corolla", 2015);
+  print("Brand: ${toyota.brand}");
+  print("Model: ${toyota.model}");
+  print("Year: ${toyota.year}");
+  print("Car Age: ${toyota.carAge()} years");
 }
